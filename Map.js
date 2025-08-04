7835295a6763494c4b30942e39e051bdf07c1ac6
SkebonElio <elionskebong@gmail.com>
2025-08-05 11:15:31 +1200

    !

---
+++ Map.js
@@ -0,0 +1,12 @@
+let OvalY = 350;
+let OvalSize;
+// vocal, drum, bass, and other are volumes ranging from 0 to 100
+function draw_one_frame(words, vocal, drum, bass, other,counter) {
+  background(20);
+  
+   map(mouseX , 0, 500, 0, 250);
+
+
+// OvalY = OvalY +1;
+
+}
